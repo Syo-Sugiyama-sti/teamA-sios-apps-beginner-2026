c0f8560a488fdddf74af4b217d4c898ebb16ea8d
s-sugiyama <s-sugiyama@sios.com>
2026-05-11 15:40:26 +0900

    ログ機能の作成

--- src/bill.py
+++ src/bill.py
@@ -10,5 +10,27 @@ items = [
     {'name': '魚', 'price': 400, 'stock': 30}
 ]
 
+Log = []
+
+def LogPrint():
+    print(f"|{'日時':^23}|{'合計金額':^6}|{'商品':^6}|{'個数':^4}|")
+    print("-" * 55)
+    for sumprice,Product,Date in Log:
+        date_str = Date.strftime("%Y-%m-%d %H:%M:%S")
+        for i in range(len(Product)):
+            if i==0:
+                print(f"|{date_str:>25}|{sumprice:>10}|{Product[i][0]:<5}|{Product[i][1]:>6}|")
+            else:
+                print(f"|{" ":>25}|{" ":>10}|{Product[i][0]:<5}|{Product[i][1]:>6}|")
+        print("-" * 55)
+        
+
 if __name__ == '__main__':
+    now = datetime.now()
+
     print('お会計システム作成')
+
+    #ログの作成
+    
+    Log.append()
+    LogPrint()
